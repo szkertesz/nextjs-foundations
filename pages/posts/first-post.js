9ce67e6c07bec0f63b819ed489e7f46cd1b96d4c
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-11-14 16:26:00 +0100

    Add 3rd party JS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 import Head from 'next/head'
+import Script from 'next/script';
 
 export default function FirstPost() {
     return <>
         <Head>
             <title>First post</title>
         </Head>
+        <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+                console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        />
         <h1>First Post</h1>
         <Link href="/">Back to home</Link>
     </>
