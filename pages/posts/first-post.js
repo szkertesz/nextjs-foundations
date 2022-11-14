ba07d39a76c746aab6491b61ccce690ca0d40674
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-11-14 16:22:23 +0100

    Add title meta

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
+import Head from 'next/head'
 
 export default function FirstPost() {
     return <>
+        <Head>
+            <title>First post</title>
+        </Head>
         <h1>First Post</h1>
         <Link href="/">Back to home</Link>
     </>
